009099aed122314dc24562d3ba506905c8940b5b
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2023-04-09 23:57:56 +0900

    [level 2] Title: 올바른 괄호, Time: 0.59 ms, Memory: 3.76 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<char> stk;
+    
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] == '(')
+            stk.push('(');
+        else
+        {
+            if(!stk.empty() && stk.top() == '(')
+                stk.pop();
+            else
+                stk.push(')');  
+        }
+    }
+    if(stk.empty())
+        return true;
+    else
+        return false;
+}
