3e533a5ffdda6344e93080386aa7b87505ffe6f9
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2023-04-28 13:54:01 +0900

    [Gold IV] Title: 알파벳, Time: 2248 ms, Memory: 10624 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.js
+++ 백준/Gold/1987. 알파벳/알파벳.js
@@ -3,38 +3,34 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
+const INF = -987654321;
 let input = [];
 let n,
+  a,
   m,
-  mx = -987654321;
-let visited = new Array(26).fill(0);
+  mx = INF;
+
 const dy = [-1, 0, 1, 0];
 const dx = [0, 1, 0, -1];
-
-function go(y, x, cnt) {
+function go(y, x, cnt, now) {
   mx = Math.max(mx, cnt);
   for (let i = 0; i < 4; i++) {
     let ny = y + dy[i];
     let nx = x + dx[i];
     if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-    let next = input[ny][nx].charCodeAt(0) - 65;
-    if (visited[next] === 0) {
-      visited[next] = 1;
-      go(ny, nx, cnt + 1);
-      visited[next] = 0;
-    }
+    let next = 1 << (a[ny][nx].charCodeAt(0) - 65);
+    if (now & next) continue;
+    else go(ny, nx, cnt + 1, now | next);
   }
+  return;
 }
 
 rl.on("line", (line) => {
   input.push(line);
 }).on("close", () => {
   [n, m] = input[0].split(" ").map(Number);
-  input.splice(0, 1);
-  // console.log(input[0][0].charCodeAt(0));
-  let now = input[0][0].charCodeAt(0) - 65;
-  visited[now] = 1;
-  go(0, 0, 1);
+  a = input.slice(1);
+  let now = 1 << (a[0][0].charCodeAt(0) - 65);
+  go(0, 0, 1, now);
   console.log(mx);
 });
