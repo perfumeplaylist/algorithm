c45ead80f07b782289a479f8b735c2f5500a1afd
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2023-06-27 15:50:54 +0900

    [Bronze III] Title: A+B - 6, Time: 156 ms, Memory: 9724 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.js
@@ -0,0 +1,17 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+rl.on("line", (line) => {
+  input.push(line);
+}).on("close", () => {
+  const n = Number(input[0]);
+  for (let i = 1; i <= n; i++) {
+    let [A, B] = input[i].split(",").map(Number);
+    console.log(A + B);
+  }
+  process.exit();
+});
