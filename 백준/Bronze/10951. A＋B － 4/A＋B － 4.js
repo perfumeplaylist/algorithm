9d776f3ff48ba8db74f59f585d98ba6a47650bdb
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2023-06-27 15:48:15 +0900

    [Bronze V] Title: A+B - 4, Time: 124 ms, Memory: 8312 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+let i = 0;
+let result = "";
+
+while (i < input.length) {
+  const num = input[i].split(" ");
+  const A = Number(num[0]);
+  const B = Number(num[1]);
+  result += A + B + "\n";
+  i++;
+}
+
+console.log(result);
