aafa0463cb2a83c9a78e95f218a63460c94fca15
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2023-06-29 12:39:24 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 184 ms, Memory: 9620 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.js
@@ -0,0 +1,14 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+rl.on("line", (line) => {
+  input.push(line);
+}).on("close", () => {
+  // let ret = "";
+  input.forEach((str) => console.log(str));
+  process.exit();
+});
