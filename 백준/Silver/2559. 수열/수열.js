659c72767d588f4019326affa658978da499cf89
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2023-07-03 16:25:25 +0900

    [Silver III] Title: 수열, Time: 240 ms, Memory: 18736 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.js
+++ 백준/Silver/2559. 수열/수열.js
@@ -3,16 +3,17 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-let input = [];
-let mx = Number.MIN_SAFE_INTEGER;
 
+let input = [];
 rl.on("line", (line) => {
   input.push(line);
 }).on("close", () => {
-  let [n,m] = input[0].split(" ").map(Number);
-  let arr = input[1].split(" ").map(Number);
-  let psum = [0];
-  for (i = 1; i <= n; i++) psum[i] = arr[i - 1] + psum[i - 1];
-  for (i = m; i <= n; i++) mx = Math.max(mx, psum[i] - psum[i - m]);
-  console.log(mx);
+  const [n, k] = input[0].split(" ").map(Number);
+  const temp = input[1].split(" ").map(Number);
+  const prev = Array.from({ length: temp.length }, () => 0);
+  let ret = -987654321;
+  for (let i = 1; i <= n; i++) prev[i] = temp[i - 1] + prev[i - 1];
+  for (let i = k; i <= n; i++) ret = Math.max(ret, prev[i] - prev[i - k]);
+  console.log(ret);
+  process.exit();
 });
