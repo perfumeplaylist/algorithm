b10bf22dc0639a4f74f377be1185ad271a101529
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2023-07-07 16:35:18 +0900

    [Silver III] Title: NBA 농구, Time: 140 ms, Memory: 9508 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.js
+++ 백준/Silver/2852. NBA 농구/NBA 농구.js
@@ -5,11 +5,17 @@ const rl = readline.createInterface({
 });
 
 let input = [];
-let asum = 0,
-  bsum = 0,
-  A = 0,
-  B = 0,
-  psum;
+let A = (B = asum = bsum = prev = 0);
+
+function changeTime(time) {
+  const min = Number(time.substring(0, 2));
+  const sec = Number(time.substring(3));
+  return min * 60 + sec;
+}
+
+function getWinerTime(score) {
+  return changeTime(score) - changeTime(prev);
+}
 
 function printTime(sum) {
   const min = "00" + Math.floor(sum / 60);
@@ -21,29 +27,20 @@ function printTime(sum) {
   );
 }
 
-function changeInt(time) {
-  const min = Number(time.substring(0, 2));
-  const sec = Number(time.substring(3));
-  return min * 60 + sec;
-}
-
-function getTime(time) {
-  return changeInt(time) - changeInt(psum);
-}
-
 rl.on("line", (line) => {
   input.push(line);
 }).on("close", () => {
   const n = Number(input[0]);
   for (let i = 1; i <= n; i++) {
-    const [team, time] = input[i].split(" ");
-    if (A > B) asum += getTime(time);
-    else if (A < B) bsum += getTime(time);
+    const [team, score] = input[i].split(" ");
+    if (A > B) asum += getWinerTime(score);
+    else if (B > A) bsum += getWinerTime(score);
     team === "1" ? A++ : B++;
-    psum = time;
+    prev = score;
   }
-  if (A > B) asum += getTime("48:00");
-  else if (A < B) bsum += getTime("48:00");
+  if (A > B) asum += getWinerTime("48:00");
+  else if (A < B) bsum += getWinerTime("48:00");
   console.log(printTime(asum));
   console.log(printTime(bsum));
+  process.exit();
 });
