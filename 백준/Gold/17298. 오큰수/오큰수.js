6288ccea01e24489704db800e499634bd9062d64
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2023-08-02 14:07:05 +0900

    [Gold IV] Title: 오큰수, Time: 1056 ms, Memory: 215640 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.js
+++ 백준/Gold/17298. 오큰수/오큰수.js
@@ -5,24 +5,25 @@ const rl = readline.createInterface({
 });
 
 let input = [];
-let stk = [];
-let ret = new Array(1000000).fill(-1);
-let p = "";
 rl.on("line", (line) => {
   input.push(line);
 }).on("close", () => {
   const n = Number(input[0]);
-  const a = input[1].split(" ").map(Number);
+  const ret = Array.from({ length: n }, () => -1);
+  const arr = input[1].split(" ").map(Number);
+  const stk = [];
+  let num = "";
   for (let i = 0; i < n; i++) {
-    let temp = a[i];
-    while (stk.length && a[stk[stk.length - 1]] < temp) {
+    let temp = arr[i];
+    while (stk.length && arr[stk[stk.length - 1]] < temp) {
       ret[stk[stk.length - 1]] = temp;
       stk.pop();
     }
     stk.push(i);
   }
   for (let i = 0; i < n; i++) {
-    p += ret[i] + " ";
+    num += ret[i] + " ";
   }
-  console.log(p);
+  console.log(num);
+  process.exit();
 });
