a34e60ba64ce456a8142eea71c18fdde47876a84
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2024-01-26 16:06:49 +0900

    [Silver III] Title: 1, Time: 196 ms, Memory: 10128 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.js
+++ 백준/Silver/4375. 1/1.js
@@ -1,26 +1,26 @@
 const readline = require("readline");
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 let input = [];
-
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  input.map(Number).forEach((n) => {
-    let cnt = 1;
-    let ret = 1;
+rl.on("line", (line) => input.push(line)).on("close", () => {
+  const num = input.map(Number);
+  for (let n of num) {
+    let cnt = 1,
+      ret = 1;
     while (true) {
-      if (cnt % n === 0) {
-        console.log(ret);
-        return;
-      } else {
-        cnt = cnt * 10 + 1;
-        cnt %= n;
-        ret++;
+      if (ret % n === 0) {
+        break;
       }
+      ret = ret * 10 + 1;
+      ret %= n;
+      cnt++;
     }
-  });
+    console.log(cnt);
+  }
+
+  process.exit();
 });
