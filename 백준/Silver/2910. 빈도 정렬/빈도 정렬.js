7d6a4eec049f9959cd6f139ad16f217089a6beea
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2024-01-29 23:21:25 +0900

    [Silver III] Title: 빈도 정렬, Time: 120 ms, Memory: 9716 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.js
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.js
@@ -1,36 +1,28 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt'
+let arr = require('fs').readFileSync(path).toString().trim().split('\n')
+arr = arr[1].split(' ').map(Number)
+let obj = {}
+let ans = []
 
-let input = [];
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  const temp = input.map((value) => value.split(" ").map(Number));
-  const [n, m] = temp[0];
-  const ret = {};
-  let retString = "";
-  for (let i = 0; i < n; i++) {
-    if (ret[temp[1][i]]) ret[temp[1][i]].cnt++;
-    else {
-      ret[temp[1][i]] = { cnt: 1, i };
+for (let i = 0; i < arr.length; i++) {
+  if (obj[arr[i]] === undefined) {
+    obj[arr[i]] = {
+      count: 1,
+      idx: i,
+      value: arr[i],
     }
+  } else {
+    obj[arr[i]].count++
   }
-  const tempObj = JSON.parse(JSON.stringify(ret));
-  const tempArray = [];
-  for (const obj in tempObj) tempArray.push({ key: obj, value: tempObj[obj] });
-  tempArray.sort((a, b) => {
-    if (a.value.cnt === b.value.cnt) return a.value.i > b.value.i ? 1 : -1;
-    return a.value.cnt > b.value.cnt ? -1 : 1;
-  });
-  for (const arr of tempArray) {
-    const {
-      key,
-      value: { cnt },
-    } = arr;
-    for (let i = 0; i < cnt; i++) retString += key + " ";
+}
+
+let res = Object.values(obj)
+res.sort((a, b) => b.count - a.count || a.idx - b.idx)
+
+for (let item of res) {
+  for (let i = 0; i < item.count; i++) {
+    ans.push(item.value)
   }
-  console.log(retString);
-});
+}
+
+console.log(ans.join(' '))
