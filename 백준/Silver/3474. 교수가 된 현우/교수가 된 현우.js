3a2536f3e4abe9b20993eca2c333a9427c7c3f97
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2024-02-01 22:09:41 +0900

    [Silver III] Title: 교수가 된 현우, Time: 240 ms, Memory: 29164 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.js
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.js
@@ -1,24 +1,20 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+{
+  const fs = require("fs");
+  const input =
+    process.platform === "linux"
+      ? fs.readFileSync("/dev/stdin").toString().trim()
+      : fs.readFileSync("bigStone/input.txt").toString().trim();
 
-let input = [];
-let n, ret2, ret5, ret;
-
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  const t = Number(input[0]);
-  for (let i = 1; i <= t; i++) {
-    n = Number(input[i]);
-    ret2 = 0;
-    ret5 = 0;
-    ret = 0;
-    for (let j = 2; j <= n; j *= 2) ret2 += Math.floor(n / j);
-    for (let k = 5; k <= n; k *= 5) ret5 += Math.floor(n / k);
-    ret = Math.min(ret2, ret5);
-    console.log(ret);
-  }
-});
+  const [N, ...arr] = input.split("\n").map(Number);
+  console.log(
+    arr
+      .map((v) => {
+        let fCnt = 0;
+        for (let i = 5; i <= v; i *= 5) {
+          fCnt += Math.floor(v / i);
+        }
+        return fCnt;
+      })
+      .join("\n")
+  );
+}
