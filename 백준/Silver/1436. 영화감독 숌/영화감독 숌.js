3aacbf63a7f14a28dbf62e5111d7fb0f064295b3
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2024-02-04 22:47:10 +0900

    [Silver V] Title: 영화감독 숌, Time: 124 ms, Memory: 9576 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -1,18 +1,30 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const N = +require('fs').readFileSync('dev/stdin').toString();
+let [n, c1, c2] = [1, 0, undefined];
+let d, t;
 
-let input = [];
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  let n = Number(input[0]);
-  let i = 666;
-  for (; ; i++) {
-    if (i.toString().search("666") !== -1) n--;
-    if (n === 0) break;
-  }
-  console.log(i);
-});
+if(N === 1) { console.log("666"); process.exit(0) }
+while(n !== N){
+    c1++; n++;
+
+    d = 0; t = c1;
+    while(t % 10 === 6 && d < 3){
+        t = Math.floor(t * 0.1);
+        d++;
+    }
+    if(d === 0){
+        c2 = undefined;
+    }else if(d >= 1){
+        c2 = 0;
+        while(n !== N && (c2 < Math.pow(10, d) - 1)){
+            c2++; n++;
+        }
+    }
+}
+
+if(c2 === undefined){
+    console.log(c1 + "666");
+}else {
+    c2 = c2.toString();
+    console.log(c1 + "666".slice(0, 3 - d) + new Array(d - c2.length + 1).join('0') + c2);
+}
+//1등코드 테스트
