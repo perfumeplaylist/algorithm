271da1f2d628d63620b597311f5c4d17a99facda
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2024-02-05 13:53:24 +0900

    [Silver IV] Title: 괄호, Time: 160 ms, Memory: 9712 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -5,26 +5,21 @@ const rl = readline.createInterface({
 });
 
 let input = [];
-
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  const n = Number(input[0]);
-  for (let i = 1; i <= n; i++) {
+rl.on("line", (line) => input.push(line)).on("close", () => {
+  const a = input.slice(1);
+  a.forEach((str) => {
     let stk = [];
-    const temp = input[i];
-    let flag = 0;
-    for (let t of temp) {
-      if (t === "(") stk.push(t);
-      else {
+    let flag = 1;
+    str.split("").forEach((char) => {
+      if (!flag) return;
+      if (char === "(") stk.push("(");
+      else if (char === ")") {
         if (stk.length) stk.pop();
-        else {
-          flag = 1;
-          break;
-        }
+        else flag = 0;
       }
-    }
-    if (flag || stk.length) console.log("NO");
+    });
+    if (!flag || stk.length) console.log("NO");
     else console.log("YES");
-  }
+  });
+  process.exit();
 });
