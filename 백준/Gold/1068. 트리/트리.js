ea1424a7a51dd2f3d939f4227e19654440b1d07b
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2024-02-06 18:26:41 +0900

    [Gold V] Title: 트리, Time: 136 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.js
+++ 백준/Gold/1068. 트리/트리.js
@@ -1,38 +1,38 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+    .toString()
+    .trim()
+    .split('\n');
 
-input.shift();
-const parentInfo = input.shift().split(' ').map(Number);
-const eraseNode = +input.shift();
 
-let tree = [];
-let cnt = 0;
-let rootNode;
+const N = +input[0];
+const parents = input[1].split(' ').map(Number);
+const removed = +input[2];
 
-parentInfo.forEach((parentNode, idx) => {
-  if (parentNode == -1) {
-    rootNode = idx;
-    return;
-  }
-  if (!tree[parentNode]) tree[parentNode] = [];
-  tree[parentNode].push(idx);
-});
 
-const dfs = (node) => {
-  if (rootNode == eraseNode) return 0;
-  if (!tree[node]) {
-    cnt++;
-    return;
-  }
-  tree[node].forEach((nodeNum) => {
-    if (nodeNum === eraseNode) {
-      if (tree[node].length === 1) cnt++;
-      return;
+const graph = Array.from({length: N}, () => []);
+for (let i = 0; i < parents.length; i++) {
+    const parent = parents[i];
+
+    if (parent > -1 && i !== removed) {
+        graph[parent].push(i)
     }
-    dfs(nodeNum);
-  });
-  return cnt;
-};
+}
 
-console.log(dfs(rootNode));
+const queue = [removed];
+let current = 0;
+
+while(queue.length !== current) {
+    const node = queue[current];
+
+    for (const child of graph[node]) {
+        queue.push(child)
+    }
+
+    graph[node] = undefined;
+
+    current++;
+}
+
+const answer = graph.reduce((pv, cv) => pv + (cv?.length === 0 ? 1 : 0), 0)
+console.log(answer)
