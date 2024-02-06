98dcc7a0698cb5687dd05b07b03a5813376a116b
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2024-02-06 17:53:08 +0900

    [Gold IV] Title: 치즈, Time: 216 ms, Memory: 11744 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.js
+++ 백준/Gold/2636. 치즈/치즈.js
@@ -5,50 +5,42 @@ const rl = readline.createInterface({
 });
 
 let input = [];
-let cheeze = [];
-let n,
-  m,
-  day = 0,
-  cnt,
-  flag = 0,
-  a,
-  ny,
-  nx,
-  visited;
 const dy = [-1, 0, 1, 0];
 const dx = [0, 1, 0, -1];
 
-function dfs(y, x) {
-  visited[y][x] = 1;
-  for (let i = 0; i < 4; i++) {
-    ny = y + dy[i];
-    nx = x + dx[i];
-    if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx]) continue;
-    if (a[ny][nx] === 0) dfs(ny, nx);
-    else if (a[ny][nx] === 1) {
-      visited[ny][nx] = 1;
-      cheeze.push({ y: ny, x: nx });
+rl.on("line", (line) => input.push(line)).on("close", () => {
+  function go(y, x, cheese, visited) {
+    visited[y][x] = 1;
+    for (let i = 0; i < 4; i++) {
+      let ny = y + dy[i];
+      let nx = x + dx[i];
+      if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx]) continue;
+      if (a[ny][nx] === 1) {
+        cheese.push({ y: ny, x: nx });
+        visited[ny][nx] = 1;
+      }
+      if (a[ny][nx] === 0) {
+        go(ny, nx, cheese, visited);
+      }
     }
   }
-}
 
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  [n, m] = input[0].split(" ").map(Number);
-  input.splice(0, 1);
-  a = input.map((el) => el.split(" ").map(Number));
+  const [n, m] = input[0].split(" ").map(Number);
+  const a = input.slice(1).map((line) => line.split(" ").map(Number));
+  let flag = 0;
+  let prev = 0;
+  let cnt = 0;
   while (true) {
-    day++;
-    cnt = 0;
     flag = 0;
-    visited = new Array(100).fill(0).map(() => new Array(100).fill(0));
-    cheeze = [];
-    dfs(0, 0);
-    for (let i of cheeze) {
-      a[i.y][i.x] = 0;
-      cnt++;
-    }
+    let cheese = [];
+    let visited = Array.from({ length: n }, () =>
+      Array.from({ length: m }, () => 0)
+    );
+    go(0, 0, cheese, visited);
+    if (!cheese.length) break;
+    for (const { y, x } of cheese) a[y][x] = 0;
+    cnt++;
+    prev = cheese.length;
     for (let i = 0; i < n; i++) {
       for (let j = 0; j < m; j++) {
         if (a[i][j] === 1) {
@@ -59,6 +51,7 @@ rl.on("line", (line) => {
     }
     if (!flag) break;
   }
-  console.log(day);
   console.log(cnt);
+  console.log(prev);
+  process.exit();
 });
