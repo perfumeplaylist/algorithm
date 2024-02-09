c10d5af2b791e6a865673e39e1145e4b7d73ad54
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2024-02-09 21:46:38 +0900

    [Gold V] Title: 보물섬, Time: 904 ms, Memory: 26888 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.js
+++ 백준/Gold/2589. 보물섬/보물섬.js
@@ -1,58 +1,44 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+let [HW, ...board] = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
-let input = [];
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  let ret = -987654321;
-  const dy = [-1, 0, 1, 0];
-  const dx = [0, 1, 0, -1];
-  let ny, nx;
-  const [n, m] = input[0].split(" ").map(Number);
-  let visited = Array.from({ length: n }, () =>
-    Array.from({ length: m }, () => 0)
-  );
-  const a = input.slice(1);
+const [H, W] = HW.split(' ').map(Number);
 
-  function bfs(y, x) {
-    const q = [];
-    visited = Array.from({ length: 54 }, () =>
-      Array.from({ length: 54 }, () => 0)
-    );
-    visited[y][x] = 1;
-    q.push({ y, x });
-    while (q.length) {
-      const { y, x } = q.shift();
-      for (let i = 0; i < 4; i++) {
-        ny = y + dy[i];
-        nx = x + dx[i];
-        if (
-          ny < 0 ||
-          nx < 0 ||
-          ny >= n ||
-          nx >= m ||
-          visited[ny][nx] ||
-          a[ny][nx] === "W"
-        )
-          continue;
-        visited[ny][nx] = 1 + visited[y][x];
-        q.push({ y: ny, x: nx });
-        ret = Math.max(ret, visited[ny][nx]);
+board = board.map((line) => line.split(''));
+
+let result = 0;
+
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+
+const search = (x, y) => {
+  const queue = [[x, y, 0]];
+  let queueIndex = 0;
+
+  const visited = Array.from({ length: H }, () => Array(W).fill(false));
+  visited[x][y] = true;
+
+  while (queueIndex < queue.length) {
+    const [cx, cy, cnt] = queue[queueIndex++];
+    result = Math.max(result, cnt);
+
+    for (let i = 0; i < 4; i++) {
+      const nx = cx + dx[i];
+      const ny = cy + dy[i];
+
+      if (nx < 0 || nx >= H || ny < 0 || ny >= W) continue;
+
+      if (!visited[nx][ny] && board[nx][ny] === 'L') {
+        visited[nx][ny] = true;
+        queue.push([nx, ny, cnt + 1]);
       }
     }
   }
+};
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (a[i][j] === "L") {
-        bfs(i, j);
-      }
-    }
+for (let i = 0; i < H; i++) {
+  for (let j = 0; j < W; j++) {
+    if (board[i][j] === 'L') search(i, j);
   }
-  console.log(ret - 1);
-  process.exit();
-});
+}
+
+console.log(result);
