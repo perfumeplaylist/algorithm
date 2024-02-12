7eb6c19ab76498593603a21a5df59b2920f70e75
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2024-02-12 21:51:47 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 168 ms, Memory: 19544 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.js
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.js
@@ -1,53 +1,80 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt'
+let arr = require('fs').readFileSync(path).toString().trim().split('\n')
 
-let input = [];
-let temp = [
+n = +arr.shift()
+arr = arr[0].split(' ').map(Number)
+let temps = [
   [9, 3, 1],
   [9, 1, 3],
   [3, 9, 1],
   [3, 1, 9],
   [1, 3, 9],
   [1, 9, 3],
-];
+]
+let temps2 = [
+  [9, 3],
+  [3, 9],
+]
+let min = 123456789
 
-let q = [];
-let visited = new Array(64)
-  .fill(0)
-  .map(() => new Array(64).fill(0).map(() => new Array(64).fill(0)));
+if (n === 3) {
+  let dp = Array.from({ length: 61 }, () =>
+    Array.from({ length: 61 }, () => Array(61).fill(0))
+  )
+  let [a, b, c] = arr
+  let q = []
+  q.push([a, b, c])
+  dp[a][b][c] = 0
+  while (q.length > 0) {
+    let [a, b, c] = q.shift()
 
-function go(a, b = 0, c = 0) {
-  visited[a][b][c] = 1;
-  q.push({ a, b, c });
-  while (q.length) {
-    let { a, b, c } = q.shift();
-    for (let i = 0; i < 6; i++) {
-      let na = Math.max(0, a - temp[i][0]);
-      let nb = Math.max(0, b - temp[i][1]);
-      let nc = Math.max(0, c - temp[i][2]);
-      if (
-        na < 0 ||
-        nb < 0 ||
-        nc < 0 ||
-        na > 60 ||
-        nb > 60 ||
-        nc > 60 ||
-        visited[na][nb][nc]
-      )
-        continue;
-      visited[na][nb][nc] = 1 + visited[a][b][c];
-      q.push({ a: na, b: nb, c: nc });
+    if (a === 0 && b === 0 && c === 0) {
+      console.log(dp[a][b][c])
+      break
+    }
+
+    for (let t of temps) {
+      let [na, nb, nc] = [a - t[0], b - t[1], c - t[2]]
+      na = na < 0 ? 0 : na
+      nb = nb < 0 ? 0 : nb
+      nc = nc < 0 ? 0 : nc
+
+      if (dp[na][nb][nc]) continue
+
+      dp[na][nb][nc] = dp[a][b][c] + 1
+      q.push([na, nb, nc])
     }
   }
-  return visited[0][0][0] - 1;
-}
+} else if (n === 2) {
+  let dp = Array.from({ length: 61 }, () => Array(61).fill(0))
+  let [a, b] = arr
+  let q = []
+  q.push([a, b])
+  dp[a][b] = 0
+  while (q.length > 0) {
+    let [a, b] = q.shift()
 
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  const [a, b, c] = input[1].split(" ").map(Number);
-  console.log(go(a, b, c));
-});
+    if (a === 0 && b === 0) {
+      console.log(dp[a][b])
+      break
+    }
+
+    for (let t of temps) {
+      let [na, nb] = [a - t[0], b - t[1]]
+      na = na < 0 ? 0 : na
+      nb = nb < 0 ? 0 : nb
+
+      if (dp[na][nb]) continue
+
+      dp[na][nb] = dp[a][b] + 1
+      q.push([na, nb])
+    }
+  }
+} else if (n === 1) {
+  let [a] = arr
+  if (a % 9 === 0) {
+    console.log(a / 9)
+  } else {
+    console.log(parseInt(a / 9) + 1)
+  }
+}
