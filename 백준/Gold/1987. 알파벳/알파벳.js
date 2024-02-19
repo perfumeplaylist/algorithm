5f66510eac74dacd7d25d44d80e0682d8807fa95
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2024-02-19 12:18:52 +0900

    [Gold IV] Title: 알파벳, Time: 1892 ms, Memory: 10512 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.js
+++ 백준/Gold/1987. 알파벳/알파벳.js
@@ -3,34 +3,34 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-const INF = -987654321;
-let input = [];
-let n,
-  a,
-  m,
-  mx = INF;
 
+let input = [];
 const dy = [-1, 0, 1, 0];
 const dx = [0, 1, 0, -1];
-function go(y, x, cnt, now) {
-  mx = Math.max(mx, cnt);
-  for (let i = 0; i < 4; i++) {
-    let ny = y + dy[i];
-    let nx = x + dx[i];
-    if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-    let next = 1 << (a[ny][nx].charCodeAt(0) - 65);
-    if (now & next) continue;
-    else go(ny, nx, cnt + 1, now | next);
-  }
-  return;
-}
 
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  [n, m] = input[0].split(" ").map(Number);
-  a = input.slice(1);
-  let now = 1 << (a[0][0].charCodeAt(0) - 65);
-  go(0, 0, 1, now);
-  console.log(mx);
+rl.on("line", (line) => input.push(line)).on("close", () => {
+  let ret = -987654321;
+
+  function go(y, x, visited, cnt) {
+    ret = Math.max(ret, cnt);
+    for (let i = 0; i < 4; i++) {
+      let ny = y + dy[i];
+      let nx = x + dx[i];
+      if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+      let next = a[ny][nx].charCodeAt(0) - 65;
+      if (!visited[next]) {
+        visited[next] = 1;
+        go(ny, nx, visited, cnt + 1);
+        visited[next] = 0;
+      }
+    }
+  }
+
+  const [n, m] = input[0].split(" ").map(Number);
+  const a = input.slice(1).map((line) => line.split(""));
+  let visited = Array.from({ length: 25 }, () => 0);
+  visited[a[0][0].charCodeAt(0) - 65] = 1;
+  go(0, 0, visited, 1);
+  console.log(ret);
+  process.exit();
 });
