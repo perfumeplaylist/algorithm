72876d808b31c71dce0bd8b5bc4c3b96e0e7e75b
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2024-02-25 14:53:29 +0900

    [Silver I] Title: 컴백홈, Time: 204 ms, Memory: 10236 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.js
+++ 백준/Silver/1189. 컴백홈/컴백홈.js
@@ -5,45 +5,32 @@ const rl = readline.createInterface({
 });
 
 let input = [];
-let r,
-  c,
-  k,
-  a,
-  visited = new Array(9).fill(0).map(() => new Array(9).fill(0));
-
 const dy = [-1, 0, 1, 0];
 const dx = [0, 1, 0, -1];
-
-function go(y, x) {
-  if (y === 0 && x === c - 1) {
-    if (visited[y][x] === k) return 1;
-    return 0;
-  }
+rl.on("line", (line) => input.push(line)).on("close", () => {
+  const [r, c, k] = input[0].split(" ").map(Number);
+  const a = input.slice(1).map((line) => line.split(""));
+  const visited = Array.from({ length: r }, () =>
+    Array.from({ length: c }, () => 0)
+  );
   let ret = 0;
-  for (let i = 0; i < 4; i++) {
-    let ny = y + dy[i];
-    let nx = x + dx[i];
-    if (
-      ny < 0 ||
-      nx < 0 ||
-      ny >= r ||
-      nx >= c ||
-      visited[ny][nx] ||
-      a[ny][nx] === "T"
-    )
-      continue;
-    visited[ny][nx] = visited[y][x] + 1;
-    ret += go(ny, nx);
-    visited[ny][nx] = 0;
+  function go(y, x, cnt, visited) {
+    if (y === 0 && x === c - 1) {
+      if (cnt === k - 1) ret++;
+      return;
+    }
+    for (let i = 0; i < 4; i++) {
+      let ny = y + dy[i];
+      let nx = x + dx[i];
+      if (ny < 0 || nx < 0 || ny >= r || nx >= c) continue;
+      if (visited[ny][nx] || a[ny][nx] === "T") continue;
+      visited[ny][nx] = 1;
+      go(ny, nx, cnt + 1, visited);
+      visited[ny][nx] = 0;
+    }
   }
-  return ret;
-}
-
-rl.on("line", (line) => {
-  input.push(line);
-}).on("close", () => {
-  [r, c, k] = input[0].split(" ").map(Number);
-  a = input.slice(1).map((el) => el.split(" ").join(""));
   visited[r - 1][0] = 1;
-  console.log(go(r - 1, 0));
+  go(r - 1, 0, 0, visited);
+  console.log(ret);
+  process.exit();
 });
