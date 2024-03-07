0898e8115819c3623b812e8d4bffca5d1ab43f0c
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2024-03-07 20:22:03 +0900

    [Gold IV] Title: Tree, Time: 4 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Gold/13244. Tree/Tree.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adj[1004];
+int t, n, m, a, b, cnt, visited[1004];
+
+void dfs(int here)
+{
+  visited[here] = 1;
+  for (int there : adj[here])
+  {
+    if (visited[there])
+      continue;
+    dfs(there);
+  }
+  return;
+}
+
+int main()
+{
+  scanf("%d", &t);
+  while (t--)
+  {
+    for (int i = 0; i < 1004; i++)
+      adj[i].clear();
+    fill(visited, visited + 1004, 0);
+    cnt = 0;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < m; i++)
+    {
+      scanf("%d %d", &a, &b);
+      adj[a].push_back(b);
+      adj[b].push_back(a);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+      if (!visited[i])
+      {
+        dfs(i);
+        cnt++;
+      }
+    }
+    if (cnt == 1 && m == n - 1)
+      puts("tree");
+    else
+      puts("graph");
+  }
+  return 0;
+}
