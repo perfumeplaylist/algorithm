0064a8b0950274deb2308cf67e4d8338817fe7b1
JoTiger <78207432+perfumeplaylist@users.noreply.github.com>
2025-06-21 16:16:17 +0900

    [level 2] Title: 타겟 넘버, Time: 13.78 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,15 +1,17 @@
-let answer = 0;
-
-function go(idx,sum,numbers,target){
-    if(idx===numbers.length){
-        if(sum===target) answer++;
-        return;
-    }    
-    go(idx+1,sum+numbers[idx],numbers,target);
-    go(idx+1,sum-numbers[idx],numbers,target);
-}
-
 function solution(numbers, target) {
-    go(0,0,numbers,target);
-    return answer;
+    let ret = 0;
+
+    function dfs(idx, sum) {
+        if (idx === numbers.length) {
+            if (sum === target) ret++;
+            return;
+        }
+
+        dfs(idx + 1, sum + numbers[idx]);
+        dfs(idx + 1, sum - numbers[idx]);
+    }
+
+    dfs(0, 0);
+
+    return ret;
 }
